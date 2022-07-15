ae806c4950d3a49642d292fd3932022f01ff0630
sunnypankaj2 <sunnypankaj2@iitkgp.ac.in>
2022-07-15 15:22:41 +0530

    all files are creted and ready to push

---
+++ server.js
@@ -0,0 +1,40 @@
+if(process.env.NODE_ENV !== 'production'){
+   require('dotenv').config();  // .load has been replace by .config
+}
+const mongoose = require ('mongoose'); 
+
+const indexRouter = require('./routes/index')  
+const authorsRouter = require('./routes/authors')  
+
+const express = require('express');  
+
+const expressLayouts = require('express-ejs-layouts'); 
+
+const bodyParser = require('body-parser')
+
+const app = express();   
+
+app.set('view engine','ejs');  
+
+app.set('views',__dirname+'/views');  
+
+app.set('layout','layouts/layout');   
+
+app.use(expressLayouts);             
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ limit: '10mb',extended: false }));
+
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true,useUnifiedTopology: true})
+
+const db = mongoose.connection;
+
+db.on('error',error=>console.error(error));
+
+db.once('open',()=>console.log("connected to mongoose"));
+
+app.listen(process.env.PORT || 3000);
+
+app.use('/',indexRouter);
+app.use('/authors',authorsRouter);
